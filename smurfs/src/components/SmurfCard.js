e48cc38bb3860c21efde7515f6d5a524b08d8764
Wes-White <wesleyrwhite86@gmail.com>
2019-11-24 19:58:56 -1000

    Added forms, Add action/reducer

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -1,25 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { getSmurf } from "../actions/action";
-import { Button } from "reactstrap";
+import { getSmurf, addSmurf } from "../actions/action";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import "./App.css";
 
 const SmurfCard = props => {
+  const [newSmurf, setNewSmurf] = useState({ name: "", age: "", height: "" });
+
+  const handleChange = e => {
+    setNewSmurf({ ...newSmurf, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.addSmurf(newSmurf);
+    props.getSmurf();
+  };
+
   return (
     <div className="smurf-card">
       <h2>SmurfCard</h2>
-      {props.loading && <div>Incoming Smurf!</div>}
+      {props.loading && <div>Incoming Smurf...</div>}
       {props.smurfs &&
         props.smurfs.map(smurf => (
-          // console.log(smurf),
-          <div key={smurf.id}>
-            <h2>{smurf.name}</h2>
-            <h2>{smurf.age}</h2>
-            <h2>{smurf.height}</h2>
+          <div className="smurf" key={smurf.id}>
+            <h2>Name: {smurf.name}</h2>
+            <h2>Age: {smurf.age}</h2>
+            <h2>Height: {smurf.height}</h2>
           </div>
         ))}
       <Button onClick={props.getSmurf} color="primary" size="lg">
         Get Smurfs
       </Button>
+      <Form inline onSubmit={handleSubmit} className="mainForm">
+        <FormGroup className="mb-3 mr-sm-3 mb-sm-0">
+          <Label for="smurfName" className="mr-sm-2">
+            Smurf's Name:
+          </Label>
+          <Input
+            onChange={handleChange}
+            placeholder="What do we call you?"
+            name="name"
+            type="text"
+          />
+        </FormGroup>
+        <FormGroup className="mb-3 mr-sm-3 mb-sm-0">
+          <Label for="smurfAge" className="mr-sm-2">
+            Smurf's Age:
+          </Label>
+          <Input
+            onChange={handleChange}
+            placeholder="How old are you? "
+            name="age"
+            type="number"
+          ></Input>
+        </FormGroup>
+
+        <FormGroup className="mb-3 mr-sm-3 mb-sm-0">
+          <Label for="smurfHeight" className="mr-sm-2">
+            Smurf's Height:
+          </Label>
+          <Input
+            onChange={handleChange}
+            placeholder="How tall are you?"
+            name="height"
+            type="text"
+          />
+        </FormGroup>
+        <Button type="sumbit" color="info">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
@@ -30,4 +81,4 @@ const mapStateToProps = state => {
     loading: state.loading
   };
 };
-export default connect(mapStateToProps, { getSmurf })(SmurfCard);
+export default connect(mapStateToProps, { getSmurf, addSmurf })(SmurfCard);
