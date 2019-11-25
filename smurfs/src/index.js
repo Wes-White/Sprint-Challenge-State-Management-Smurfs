6ffc90c16abe30ebaf669542a5b79ecf50dd4da9
Wes-White <wesleyrwhite86@gmail.com>
2019-11-24 17:50:11 -1000

    Setup Redux, building reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducer from "./reducers/reducer";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(logger, thunk));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  error: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
